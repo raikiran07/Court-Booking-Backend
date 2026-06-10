b7dda32c149fb9fcf0c6cf03e13255f68ede422f
Kiran Rai <84770797+raikiran07@users.noreply.github.com>
2026-06-10 14:31:45 +0530

    chore: update the default port to 3000 and updated few library version

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ cron.schedule('*/5 * * * *', async () => {
   await Slot.updateMany({ _id: { $in: slotIds } }, { status: 'available' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
